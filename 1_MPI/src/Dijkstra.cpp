261d91e3e9d8d2425cf4a844c479e026fc1d00cc
Jakub Szydełko <kubarroo248@interia.pl>
2026-04-11 20:34:39 +0200

    Added Dijkstra Single Source Shortest Path algorithm

--- 1_MPI/src/Dijkstra.cpp
+++ 1_MPI/src/Dijkstra.cpp
@@ -1,12 +1,14 @@
+#include <climits>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <utility>
 #include <vector>
-#include <unordered_map>
-#include "Edge.hpp"
+#include <queue>
 
-void ImportGraph(std::string, std::vector<int>&, std::unordered_map<Edge, int>&);
+void ImportGraph(const std::string&, std::vector<std::vector<std::pair<int, int>>>&);
+std::vector<int> DijkstraSingleSourceSP(const std::vector<std::vector<std::pair<int, int>>>&, int);
 
 int main(int argc, char *argv[])
 {
@@ -16,47 +18,88 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    std::vector<int> V; 
-    std::unordered_map<Edge, int> E;
-    // std::string filePath{argv[1]};
-    ImportGraph(argv[1], V, E);
+    std::vector<std::vector<std::pair<int, int>>> E;
+    ImportGraph(argv[1], E);
 
-    for (const auto& [edge, value] : E) {
-        std::cout << "(" << edge.a << ", " << edge.b << ") -> " << value << "\n";
+    std::cout << "Edges (from, to) -> weight:\n";
+    for (int i = 0; i < E.size(); i++)
+    {
+        for (const auto& [v, w] : E[i]) {
+            std::cout << "(" << i << ", " << v << ") -> " << w << "\n";
+        }
+    }
+
+    auto l = DijkstraSingleSourceSP(E, 0);
+
+    std::cout << "Shortest paths:\n";
+    for (int i = 0; i < l.size(); i++)
+    {
+        std::cout << "vert " << i << " shortest path: ";
+        if (l[i] == INT_MAX)
+            std::cout << "INF" << std::endl;
+        else
+            std::cout << l[i] << std::endl;
     }
 
     return 0;
 }
 
-void ImportGraph(std::string filePath, std::vector<int>& V, std::unordered_map<Edge, int>& E)
+void ImportGraph(const std::string& filePath, std::vector<std::vector<std::pair<int, int>>>& E)
 {
     std::cout << "Importing graph from file: " << filePath << std::endl; 
     std::ifstream file(filePath);
-    std::string line;
-    int i, j;
-    i = j = 0;
-    while (std::getline(file, line))
+    if (!file)
     {
-        V.push_back(i);
+        throw std::runtime_error("Could not open file: " + filePath);
+    }
+
+    std::string line;
+    for (int i = 0; std::getline(file, line); i++)
+    {
+        E.emplace_back();
         std::stringstream lineStream(line);
         std::string valStr;
-        while (std::getline(lineStream, valStr, '\t'))
+        for (int j = 0; std::getline(lineStream, valStr, '\t'); j++)
         {
-            int val = std::atoi(valStr.c_str());
+            int val = std::stoi(valStr);
             if (val > 0)
-                E.insert({Edge{i, j}, val});
+                E[i].emplace_back(j, val);
 
             std::cout << val << "\t";
-            j++;
         }
         std::cout << std::endl;
-        j = 0;
-        i++;
     }
 }
 
 
-void DijkstraSingleSourceSP()
+std::vector<int> DijkstraSingleSourceSP(const std::vector<std::vector<std::pair<int, int>>>& E, int s)
 {
+    int n = E.size();
+    std::vector<int> l(n, INT_MAX);
 
+    using P = std::pair<int,int>; // {dist, vertex}
+    std::priority_queue<P, std::vector<P>, std::greater<P>> pq;
+
+    l[s] = 0;
+    pq.push({l[s], s});
+
+    while (!pq.empty())
+    {
+        auto [d, u] = pq.top();
+        pq.pop();
+
+        if (d != l[u])
+            continue;
+
+        for (auto [v, w] : E[u])
+        {
+            if (l[u] != INT_MAX && l[u] + w < l[v])
+            {
+                l[v] = l[u] + w;
+                pq.push({l[v], v});
+            }
+        }
+    }
+
+    return l;  
 }
